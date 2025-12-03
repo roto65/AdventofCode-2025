1ffec29a03e0fbadf231d76baa41b44372c249fe
roto65 <antoniorotondo65@gmail.com>
2025-12-03 16:48:55 +0100

    Bugfix

--- src/solver/shared.rs
+++ src/solver/shared.rs
@@ -2,12 +2,12 @@ use std::{fs, time::Instant};
 
 pub fn read_input(num: u32, test: bool) -> Vec<String> {
     let content = if test {
-        fs::read_to_string(format!("data/day{}.test.txt", num)).unwrap()
+        fs::read_to_string(format!("data/day{:02}.test.txt", num)).unwrap()
     } else {
-        fs::read_to_string(format!("data/day{}.txt", num)).unwrap()
+        fs::read_to_string(format!("data/day{:02}.txt", num)).unwrap()
     };
 
-    return content.split("\n").into_iter().map(String::from).collect();
+    return content.split("\n").into_iter().map(str::trim).map(String::from).collect();
 }
 
 pub fn timer<F, R>(f: F) -> R
